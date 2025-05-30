7beee9654db05c086f295701aed084369cc7f8a3
Electrifier <samchijioke16@gmail.com>
2025-05-30 20:20:11 +0100

    refactor: Create `DownloadFileButton` component to use signed URLs for file downloads

---
+++ src/pages/Messaging/components/downloadButton.tsx
@@ -0,0 +1,30 @@
+import { RxDownload } from "react-icons/rx"
+import useAwsFile from "@hooks/useAwsFile";
+import { Data as ChatData } from "type/api/messaging.types";
+
+interface Props {
+    newItem: ChatData;
+}
+
+const DownloadFileButton = ({ newItem }: Props) => {
+    const { downloadFile } = useAwsFile({
+        opened: true,
+        attachDocument: newItem.metadata.key
+    });
+
+    return (
+        <div
+            className="flex  items-center "
+            onClick={downloadFile}
+        >
+            <RxDownload
+                color="#D95B0E"
+                size={22}
+                style={{ marginRight: "5px" }}
+            />
+            <p className="cursor-pointer text-sm">Download</p>
+        </div>
+    );
+}
+
+export default DownloadFileButton;
